bd4f66c3e92a269579bbaf1f56b7d9de1ba53918
mw <77810941+malteww@users.noreply.github.com>
2021-04-21 12:07:32 +0200

    Update Database.java

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -11,19 +11,6 @@ import java.util.TreeMap;
 import static krusty.Jsonizer.toJson;
 
 public class Database {
-	/**
-	 * Modify it to fit your environment and then use this string when connecting to your database!
-	 */
-	private static final String jdbcString = "jdbc:mysql://localhost/krusty";
-
-	// For use with MySQL or PostgreSQL
-	private static final String jdbcUsername = "localhost";
-	private static final String jdbcUsername = "root";
-	private static final String jdbcPassword = "localhost";
-
-	public void connect() {
-		// Connect to database here
-	}
     /**
      * Modify it to fit your environment and then use this string when connecting to your database!
      */
@@ -42,11 +29,6 @@ public class Database {
         }
     }
 
-	// TODO: Implement and change output in all methods below!
-
-	public String getCustomers(Request req, Response res) {
-		return "{}";
-	}
     // TODO: Implement and change output in all methods below!
 
     public String getCustomers(Request req, Response res) {
@@ -54,51 +36,32 @@ public class Database {
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ResultSet rs = ps.executeQuery();
             return Jsonizer.toJson(rs, "customers");
-
         } catch (SQLException exception) {
             exception.printStackTrace();
             return Jsonizer.anythingToJson(exception.getMessage(), "status");
         }
     }
 
-	public String getRawMaterials(Request req, Response res) {
-		return "{}";
-	}
     public String getRawMaterials(Request req, Response res) {
         return "{}";
     }
 
-	public String getCookies(Request req, Response res) {
-		return "{\"cookies\":[]}";
-	}
     public String getCookies(Request req, Response res) {
         return "{\"cookies\":[]}";
     }
 
-	public String getRecipes(Request req, Response res) {
-		return "{}";
-	}
     public String getRecipes(Request req, Response res) {
         return "{}";
     }
 
-	public String getPallets(Request req, Response res) {
-		return "{\"pallets\":[]}";
-	}
     public String getPallets(Request req, Response res) {
         return "{\"pallets\":[]}";
     }
 
-	public String reset(Request req, Response res) {
-		return "{}";
-	}
     public String reset(Request req, Response res) {
         return "{}";
     }
 
-	public String createPallet(Request req, Response res) {
-		return "{}";
-	}
     public String createPallet(Request req, Response res) {
         return "{}";
     }
