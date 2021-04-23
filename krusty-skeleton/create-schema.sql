a9857d8ad96c587f5b295d3a23b784d37fc65a8f
mw <77810941+malteww@users.noreply.github.com>
2021-04-23 11:19:11 +0200

    Flyttade SQL-statementsen för att fylla databasen till intital-data.sql

--- krusty-skeleton/create-schema.sql
+++ krusty-skeleton/create-schema.sql
@@ -66,45 +66,4 @@ FOREIGN KEY(CookieName) REFERENCES Cookies(CookieName),
 FOREIGN KEY(OrderNbr) REFERENCES Orders(OrderNbr)
 );
 
--- fyll databasen för testen 
-INSERT INTO customer(CustomerName, CustomerAddress)
-VALUES("Bjudkakor AB", "Ystad"),
-    ("Finkakor AB", "Helsingborg"),
-    ("Gästkakor AB",  "Hässleholm"),
-     ("Kaffebröd AB", "Landskrona"),
-    ("Kalaskakor AB","Trelleborg"),
-    ("Partykakor AB","Kristianstad"),
-     ("Skånekakor AB","Perstorp"),
-    ("Småbröd AB",  "Malmö");
-
-INSERT INTO Storage(IngredientName, StockAmount, Unit, AmountDelivered, TimeDelivered)
-VALUES("Bread crumbs", 500000,  "g", null, null),
-("Butter", 500000,  "g", null, null),
-("Chocolate", 500000,"g", null, null),
-("Chopped almonds", 500000,"g", null, null),
-("Cinnamon", 500000, "g", null, null),
-("Egg whites", 500000, "ml", null, null),
-("Eggs", 500000, "g", null, null),
-("Fine-ground nuts", 500000, "g", null, null),
-("Flour", 500000,  "g", null, null),
-("Ground, roasted nuts", 500000,"g", null, null),
-("Icing sugar", 500000, "g", null, null),
-("Marzipan", 500000, "g", null, null),
-("Potato starch", 500000, "g", null, null),
-("Roasted, chopped nuts", 500000, "g", null, null),
-("Sodium bicarbonate", 500000, "g", null, null),
-("Sugar",  500000, "g", null, null),
-("Vanilla", 500000, "g", null, null),
-("Vanilla sugar",  500000, "g", null, null),
-("Wheat flour", 500000, "g", null, null);
-
-INSERT INTO Cookies(CookieName)
-VALUES("Almond delight"),
-("Amneris"),
-("Berliner"),
-("Nut cookie"),
-("Nut ring"),
-("Tango")
-;
-
 SET FOREIGN_KEY_CHECKS=1;
