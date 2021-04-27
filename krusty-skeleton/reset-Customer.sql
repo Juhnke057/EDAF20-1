82e039e6aaf9efb763a756a40dfe639484a4a13f
mw <77810941+malteww@users.noreply.github.com>
2021-04-27 17:30:34 +0200

    SQL reset filer

---
+++ krusty-skeleton/reset-Customer.sql
@@ -0,0 +1,9 @@
+INSERT INTO krusty.Customer(CustomerName, CustomerAddress)
+VALUES("Bjudkakor AB", "Ystad"),
+    ("Finkakor AB", "Helsingborg"),
+    ("Gästkakor AB",  "Hässleholm"),
+     ("Kaffebröd AB", "Landskrona"),
+    ("Kalaskakor AB","Trelleborg"),
+    ("Partykakor AB","Kristianstad"),
+     ("Skånekakor AB","Perstorp"),
+    ("Småbröd AB",  "Malmö");
