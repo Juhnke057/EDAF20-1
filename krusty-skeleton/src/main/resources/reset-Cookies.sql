7fe5d1998223894ff24dff3131a0d994cfed39d7
mw <77810941+malteww@users.noreply.github.com>
2021-05-02 10:40:22 +0200

    Revert "flyttade reset.sql till main/resources"
    This reverts commit fb21f734ed156c1ebee1b937b7f4c5135044accd.

--- krusty-skeleton/src/main/resources/reset-Cookies.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO krusty.Cookies(CookieName)
-VALUES("Almond delight"),
-("Amneris"),
-("Berliner"),
-("Nut cookie"),
-("Nut ring"),
-("Tango");
--- krusty-skeleton/src/main/resources/reset-Customer.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO krusty.Customer(CustomerName, CustomerAddress)
-VALUES("Bjudkakor AB", "Ystad"),
-    ("Finkakor AB", "Helsingborg"),
-    ("Gästkakor AB",  "Hässleholm"),
-     ("Kaffebröd AB", "Landskrona"),
-    ("Kalaskakor AB","Trelleborg"),
-    ("Partykakor AB","Kristianstad"),
-     ("Skånekakor AB","Perstorp"),
-    ("Småbröd AB",  "Malmö");
