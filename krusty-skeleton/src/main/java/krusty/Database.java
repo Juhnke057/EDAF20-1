78564fa26d7ab320706f1e8ffba5bf8d9a0442f2
mw <77810941+malteww@users.noreply.github.com>
2021-05-04 14:17:45 +0200

    Tog bort onödiga import statements

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -7,11 +7,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class Database {
